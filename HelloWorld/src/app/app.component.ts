e126cfa493b210f7976c49184388d9edf73c0dc0
ry129 <0111cs211146.riya@gmail.com>
2025-03-06 21:48:54 +0530

    [Riya]Add. Username validated

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent {
   imgUrl = '../logo.jpg';
   url = 'https://www.bridgelabz.com';
   userName = '';
+  nameError: String = '';
 
   ngOnInit(): void {
     this.title = 'Hello from Bridgelabz.';
@@ -21,5 +22,14 @@ export class AppComponent {
   onClick($event: Event) {
     console.log('Save button is clicked!', $event);
     window.open(this.url, '_blank');
+  }
+  onInput($event: Event) {
+    console.log('Change Event Occurred!', $event);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if (nameRegex.test(this.userName)) {
+      this.nameError = '';
+      return;
+    }
+    this.nameError = 'Name is Incorrect!';
   }
 }
