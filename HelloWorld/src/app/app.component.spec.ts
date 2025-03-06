056e33682484196f9e957899348787089cde44d3
ry129 <0111cs211146.riya@gmail.com>
2025-03-06 21:33:59 +0530

    [Riya]Add. Display Hello and Name by user Input

--- HelloWorld/src/app/app.component.spec.ts
+++ HelloWorld/src/app/app.component.spec.ts
@@ -1,29 +1,14 @@
-import { TestBed } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { routes } from './app.routes';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'HelloWorld' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('HelloWorld');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, HelloWorld');
-  });
-});
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, routes],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
